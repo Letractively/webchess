e392703b9319f12ab42143fa4eaf3b0bfc82cd92
whwqs.hb <whwqs.hb@gmail.com>
2011-09-12 14:44:13 +0000

    框架完善中

--- webchess/src/com/whwqs/util/IPublisher.java
+++ webchess/src/com/whwqs/util/IPublisher.java
@@ -1,7 +1,7 @@
 package com.whwqs.util;
 
 public interface IPublisher {
-	void AddSubscriber(ISubscriber subscriber);
-	void RemoveSubscriber(ISubscriber subscriber);
-	void Notify(EventBase eventArg);
+	void AddSubscriber(String EventName,ISubscriber subscriber);
+	void RemoveSubscriber(String EventName,ISubscriber subscriber);
+	void Notify(String EventName,EventBase eventArg);
 }
---
+++ webchess/src/com/whwqs/webchess/core/ChessEvent.java
@@ -0,0 +1,88 @@
+package com.whwqs.webchess.core;
+
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public class ChessEvent extends  com.whwqs.util.EventBase {
+	public static final String EVENT_COMPLETECHECKBOARD = "棋盘检查完成";
+	public static final String EVENT_BOARDEXPIRE = "检查到操作时棋盘数据过期";		
+	public static final String EVENT_GAME_START = "棋局开始";
+	public static final String EVENT_PLAY = "落子";
+	public static final String EVENT_UNDO = "悔棋";
+	public static final String EVENT_REDO = "不悔了";
+	public static final String EVENT_END = "棋局结束";
+	public static final String EVENT_TIMEOUT = "棋手超时";
+	public ChessEvent(String EventName)
+	{
+		super(EventName,null);
+		this.eventName = EventName;
+	}
+	private String eventName = "";
+	private String wrongNodesPositionData = "";//检查棋盘是否符合规则 格式1|2|3...	
+	private String chessBoardData = ""; //车马相士帅...
+	private Date gameStartAt;
+	private Date gameEndAt;
+	private Date playAt;
+	private Boolean isRedToGo;
+	public Boolean getIsRedToGo() {
+		return isRedToGo;
+	}
+	public void setIsRedToGo(Boolean isRedToGo) {
+		this.isRedToGo = isRedToGo;
+	}
+	private int fromNode = -1;
+	private int toNode = -1;
+	private String message;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getWrongNodesPositionData() {
+		return wrongNodesPositionData;
+	}
+	public void setWrongNodesPositionData(String wrongNodesPositionData) {
+		this.wrongNodesPositionData = wrongNodesPositionData;
+	}
+	public String getChessBoardData() {
+		return chessBoardData;
+	}
+	public void setChessBoardData(String chessBoardData) {
+		this.chessBoardData = chessBoardData;
+	}
+	public Date getGameStartAt() {
+		return gameStartAt;
+	}
+	public void setGameStartAt(Date gameStartAt) {
+		this.gameStartAt = gameStartAt;
+	}
+	public Date getGameEndAt() {
+		return gameEndAt;
+	}
+	public void setGameEndAt(Date gameEndAt) {
+		this.gameEndAt = gameEndAt;
+	}
+	public Date getPlayAt() {
+		return playAt;
+	}
+	public void setPlayAt(Date playAt) {
+		this.playAt = playAt;
+	}
+	public int getFromNode() {
+		return fromNode;
+	}
+	public void setFromNode(int fromNode) {
+		this.fromNode = fromNode;
+	}
+	public int getToNode() {
+		return toNode;
+	}
+	public void setToNode(int toNode) {
+		this.toNode = toNode;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	
+}
