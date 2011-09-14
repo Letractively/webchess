201f70e487af36c304d743bf47e1d9d217c30a91
whwqs.hb <whwqs.hb@gmail.com>
2011-09-14 10:18:02 +0000

    unit test ready

---
+++ webchess/src/com/whwqs/webchess/ChessBoardManager.java
@@ -0,0 +1,14 @@
+package com.whwqs.webchess;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import com.whwqs.webchess.core.ChessBoard;
+
+public class ChessBoardManager {
+	private static String root = "c:/chess";	
+	private static Map<String,ChessBoard> ChessBoards = new Hashtable<String,ChessBoard>();
+	static{
+		java.io.f
+	}
+}
