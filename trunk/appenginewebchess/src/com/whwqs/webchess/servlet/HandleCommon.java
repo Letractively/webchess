a40ae6bcc34deb56d4c5f0edb12b95478565046c
whwqs.hb@gmail.com <whwqs.hb@gmail.com@1f505f3f-e414-6a9e-f7b9-98d5ac2c3e42>
2011-11-02 09:26:06 +0000

    google app engine

--- trunk/appenginewebchess/src/com/whwqs/webchess/servlet/HandleCommon.java
+++ trunk/appenginewebchess/src/com/whwqs/webchess/servlet/HandleCommon.java
@@ -2,7 +2,6 @@ package com.whwqs.webchess.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import com.whwqs.webchess.ChessBoardManager;
 /**
  * Servlet implementation class HandleCommon
  */
-@WebServlet("/HandleCommon")
+
 public class HandleCommon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- trunk/appenginewebchess/war/welcome.jsp
+++ trunk/appenginewebchess/war/welcome.jsp
@@ -2,11 +2,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@page import="com.whwqs.webchess.*, com.whwqs.webchess.core.*"  %>
- <%
- 	String root = request.getSession().getServletContext().getRealPath("chess");
-	ChessBoardManager.setRoot(root);
-	System.out.println(root);
-%>
+ 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
